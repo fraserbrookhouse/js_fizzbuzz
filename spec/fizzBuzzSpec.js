68a73451137816e28698719996318911c80f30e5
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-09-01 16:42:56 +0100

    added test for DivisibleByThree to return false when it is not, fails test

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -9,4 +9,12 @@ describe('Fizzbuzz', function () {
       expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
     });
   });
+
+  describe('knows when a number is NOT', function () {
+
+    it('divisible by 3', function() {
+      fizzbuzz = new Fizzbuzz();
+      expect(fizzbuzz.isDivisibleByThree(1)).toBe(false);
+    });
+  });
 });
