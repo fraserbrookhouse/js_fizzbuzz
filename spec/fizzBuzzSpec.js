4fbbf8ad786b83b77a65260aec76e452f4c5f853
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-09-01 16:52:35 +0100

    added tests for DivisibleByFive method, fails tests

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -8,6 +8,11 @@ describe('Fizzbuzz', function () {
       fizzbuzz = new Fizzbuzz();
       expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
     });
+
+    it('divisible by 5', function() {
+      fizzbuzz = new Fizzbuzz();
+      expect(fizzbuzz.isDivisibleByFive(5)).toBe(true);
+    });
   });
 
   describe('knows when a number is NOT', function () {
@@ -15,6 +20,11 @@ describe('Fizzbuzz', function () {
     it('divisible by 3', function() {
       fizzbuzz = new Fizzbuzz();
       expect(fizzbuzz.isDivisibleByThree(1)).toBe(false);
+    });
+
+    it('divisible by 5', function() {
+      fizzbuzz = new Fizzbuzz();
+      expect(fizzbuzz.isDivisibleByFive(5)).toBe(false);
     });
   });
 });
